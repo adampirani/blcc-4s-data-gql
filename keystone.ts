3618a8369b81aac7ec6d521433d31a69804df1e9
Adam Pirani <adam.pirani@gmail.com>
2021-08-30 12:01:57 -0400

    Add local files

---
+++ keystone.ts
@@ -0,0 +1,67 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long to stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // Seed data if flag present
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show the UI only for ppl who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id name email',
+    }),
+  })
+);
