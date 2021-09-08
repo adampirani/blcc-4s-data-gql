d0400ce97b59e2b85239aa316c0a574dd3361d20
Adam Pirani <adam.pirani@gmail.com>
2021-09-08 10:04:51 -0400

    Added user-->teams relationship

--- schemas/User.ts
+++ schemas/User.ts
@@ -1,5 +1,5 @@
 import { list } from '@keystone-next/keystone/schema';
-import { text, password } from '@keystone-next/fields';
+import { text, password, relationship } from '@keystone-next/fields';
 
 export const User = list({
   // access:
@@ -8,6 +8,10 @@ export const User = list({
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
     password: password(),
+    teams: relationship({
+      ref: 'Team',
+      many: true,
+    }),
     // TODO: add roles, teams
   },
 });
